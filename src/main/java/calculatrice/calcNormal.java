74ba100039c8f876abf76906b088b619b15d0a22
Squlalah <44104621+Squlalah@users.noreply.github.com>
2019-02-11 17:29:36 +0100

    Calc normal (#3)
    * Ajout addition
    
    * Ajout multiplication
    
    * Modification calcNormal pour passer les fonctions en static

--- src/main/java/calculatrice/calcNormal.java
+++ src/main/java/calculatrice/calcNormal.java
@@ -1,10 +1,21 @@
 package calculatrice;
 
+import org.apache.commons.math3.*;
+
+
 public class calcNormal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public calcNormal() {
+	}
+	
+	public static double add(double val1, double val2) {
+		org.apache.commons.math3.analysis.function.Add add = new org.apache.commons.math3.analysis.function.Add();
+		return add.value(val1, val2);
+	}
+	
+	public static double mul(double val1, double val2) {
+		org.apache.commons.math3.analysis.function.Multiply mul = new org.apache.commons.math3.analysis.function.Multiply();
+		return mul.value(val1, val2);
 	}
 
 }
