6e0ee28c6c9871b0248150d83c8927806d35b747
clement bourgeault <16013153@e105m06.istic.univ-rennes1.fr>
2019-02-11 16:23:55 +0100

    modification choix du mode

--- src/main/java/calculatrice/App.java
+++ src/main/java/calculatrice/App.java
@@ -1,5 +1,7 @@
 package calculatrice;
 
+import java.util.Scanner;
+
 import org.apache.commons.math3.*;
 /**
  * Hello world!
@@ -10,19 +12,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Choisissez le mode normal(n) ou scientifique(s)");
+    	Scanner sc = new Scanner(System.in);
+    	boolean choix = false;
+    	
+        while(!choix) {
+        	System.out.println("Choisissez le mode normal(n) ou scientifique(s)");
         	
-        if(args[0].equals("n") || args[0].equals("N")) {
-        	
+	        String str = sc.nextLine();
+	        
+	        if(str.equals("n") || str.equals("N")) {
+	        	System.out.println("Mode Normal");
+	        	choix = true;
+	        }
+	        else if (str.equals("s") || str.equals("S")) {
+	        	System.out.println("Mode Scientifique");
+	        	choix = true;
+	        }
+	        else {
+	        	System.out.println("Saisie incorrecte ! Reessayez");
+	        }
         }
-        else {
-        	if(args[0].equals("s") || args[0].equals("S")) {
-        		
-        	}
-        	else {
-        		System.out.println("Saisie incorrecte! Reessayez");
-        	}
-        }
-        
+        sc.close();
     }
 }
