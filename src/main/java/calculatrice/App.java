6fb82bf4a18388fa2eecc9dc084e6108c79c5f03
Squlalah <16013153@e212m05.istic.univ-rennes1.fr>
2019-03-05 17:02:36 +0100

    fix app

--- src/main/java/calculatrice/App.java
+++ src/main/java/calculatrice/App.java
@@ -7,72 +7,82 @@ import org.apache.commons.math3.*;
  * Calculatrice App
  *
  */
- 
+
 public class App 
 {
-public static int init(Scanner sc) {
-			
-			boolean choix=false;
-			int tmp=-1;
-			System.out.println("Choisissez le mode normal(n) ou scientifique(s)");
-        	
-       		 while(!choix) {
-        	
-        
-	        String str = sc.nextLine();
-	        
-	        if(str.equals("n") || str.equals("N")) {
+	public static int init(Scanner sc) {
+
+		boolean choix=false;
+		int tmp=-1;
+		System.out.println("Choisissez le mode normal(n) ou scientifique(s)");
+
+		while(!choix) {
+
+
+			String str = sc.nextLine();
+
+			if(str.equals("n") || str.equals("N")) {
 				System.out.println("Mode Normal");
 				tmp=0;
-	        	choix = true;
-	        }
-	        else if (str.equals("s") || str.equals("S")) {
+				choix = true;
+			}
+			else if (str.equals("s") || str.equals("S")) {
 				System.out.println("Mode Scientifique");
 				tmp=1;
-	        	choix = true;
-	        }
-	        else {
-	        	System.out.println("Saisie incorrecte ! Reessayez");
+				choix = true;
+			}
+			else {
+				System.out.println("Saisie incorrecte ! Reessayez");
 			}
 		}	
-		
-    	
-	return tmp;
-}
+
+
+		return tmp;
+	}
 
 	public static void main( String[] args )
-    {
+	{
 		Scanner sc = new Scanner(System.in);
 		int i=init(sc);
-		if(i==0){
-			cal= new calcNormal();
+		boolean scien=false;
+		if(i==1){
+			scien=true;
 		}
-		else{
-			cal= new calcScien();
-		}
+		
 		while(true){
-		System.out.println("entrez votre calcule");
-		String enter=sc.nextLine();
-		String[] entersplit=enter.split(" ");
-		String tmp2=enter.split("[\\(||//)]")[0];
-		String tmp3=enter.split("(")[0];
-		int number1=Integer.parseInt(entersplit[0]);
-		int number2=Integer.parseInt(entersplit[2]);
-		String operande=entersplit[1];
-			switch(operande){
-				case "+":
-				System.out.println(cal.add(number1,number2));
-				case "*":
-				System.out.println(cal.mul(number1,number2));					
+			System.out.println("entrez votre calcul");
+			String enter=sc.nextLine();
+			
+			if(!scien) {
+				String[] entersplit=enter.split(" ");
+				
+				int number1=Integer.parseInt(entersplit[0]);
+				int number2=Integer.parseInt(entersplit[2]);
+
+				String operande=entersplit[1];
+				if(operande.equals("+")) {
+					System.out.println(calcNormal.add(number1,number2));
+				}
+				if(operande.equals("*")) {
+					System.out.println(calcNormal.mul(number1,number2));
+				}
 			}
-			if(tmp3.equals("sqrt")){
-				System.out.println(cal.RacineCarree(Integer.parseInt(tmp2)));
-			}
-			if(tmp3.equals("ArcCosinus")){
-				System.out.println(cal.ArcCosinus(Integer.parseInt(tmp2)));
+			if(scien) {
+				
+				String tmp2=enter.split("[\\(||//)]")[1];
+				String tmp3=enter.split("[\\(||//)]")[0];
+				
+				
+				if(tmp3.equals("sqrt")){
+					
+					System.out.println(calcScien.RacineCarree(Integer.parseInt(tmp2)));
+				}
+				if(tmp3.equals("ArcCosinus")){
+					System.out.println(calcScien.ArcCosinus(Integer.parseInt(tmp2)));
+				}
 			}
 		}
-    	
-        //sc.close();
-    }
+
+		//sc.close();
+	}
 }
